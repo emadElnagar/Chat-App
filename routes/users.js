4f26197785cf9448cf2052bd71e9b0a506fc4a6d
unknown <emad.m.alnagar@gmail.com>
2022-04-14 01:16:00 +0200

    login and register page

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/login', (req, res) => {
+  res.render('user/login.ejs', {title: 'Chat'});
+});
+
+router.get('/register', (req, res) => {
+  res.render('user/register.ejs', {title: 'Chat'});
+});
+
 module.exports = router;
