08c045483f3dde1167c224e99945e8978184ec41
unknown <emad.m.alnagar@gmail.com>
2022-06-13 19:42:18 +0200

    admin login backend

--- routes/admin.js
+++ routes/admin.js
@@ -17,3 +17,34 @@ router.get('/login', (req, res) => {
     res.render('admin/signin', { title: 'Chat-admin-login', messages: errorMessage });
   }
 });
+
+router.post('/login', async(req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const loginError = `you arent allowed here or wrong password or email`;
+  if(!user) {
+    req.flash('loginError', loginError);
+    res.redirect('login');
+    return;
+  }
+  const validate = await bcrypt.compare(req.body.password, user.password);
+  if (!validate) {
+    req.flash('loginError', loginError);
+    res.redirect('login');
+    return;
+  }
+  if (user.isAdmin) {
+    loggedin = req.session;
+    loggedin.user = {};
+    loggedin.user.email = req.body.email;
+    loggedin.user._id = user._id;
+    loggedin.user.firstName = user.firstName;
+    loggedin.user.lastName = user.lastName;
+    loggedin.user.gender = user.gender;
+    loggedin.user.isAdmin = user.isAdmin;
+    loggedin.user.image = user.profileImg;
+    res.redirect('/admin');
+  } else {
+    req.flash('loginError', loginError);
+    res.redirect('login');
+  }
+});
