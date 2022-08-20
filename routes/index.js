830392e464c47517ae74e97e57d93880d4071a1d
unknown <emad.m.alnagar@gmail.com>
2022-08-20 22:46:49 +0200

    search users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.session.user });
+  res.render('index', { title: 'Chat', user: req.session.user });
+});
+
+// SEARCH
+router.get('/search', async (req, res) => {
+  const { search } = req.query;
+  const searchedUsersf = await User.find({ firstName: search });
+  const searchedUsersl = await User.find({ lastName: search });
+  if (searchedUsersf.length === 0 && searchedUsersl.length === 0) {
+    res.render('search/not-found', { title: 'Chat-search', user: req.session.user });
+  } else {
+    res.render('search/search', {
+      title: 'Chat-search',
+      user: req.session.user,
+      searchedUsersf,
+      searchedUsersl,
+    })
+  }
 });
 
 module.exports = router;
